da9439b00a234790b3ee11045940c8f8478f8368
Jackon Yang <jiekunyang@gmail.com>
2013-03-28 16:19:16 +0800

    provide more public method.

--- message.py
+++ message.py
@@ -3,27 +3,27 @@ format to show"""
 
 line_length = 60
 
-def __wrap_infomation(info):
+def wrap(info):
 	start_length = int((line_length - len(info)) / 2)
 	return "{}{}{}\n".format('-' * start_length, info,
 		'-' * (line_length - start_length - len(info)))
 
-def __stars_line():
+def stars_line():
 	return '*' * line_length + '\n'
 def clear_screen(cmd):
 	import os
 	os.system(cmd)
 
 def head(*args, **kwargs):
-	head_info = __stars_line()
+	head_info = stars_line()
 	for info in args:
-		head_info += __wrap_infomation(info)
+		head_info += wrap(info)
 	for info in kwargs.values():
-		head_info += __wrap_infomation(info)
-	head_info += __stars_line()
+		head_info += wrap(info)
+	head_info += stars_line()
 	return head_info
 
 if __name__ == "__main__":
-	#print(tt.wrap_infomation('dict'))
+	#print(tt.wrap('dict'))
 	#print(tt.stars())
 	print(head("haithink's Dict", contant='Contact 330240295@qq.com'))
